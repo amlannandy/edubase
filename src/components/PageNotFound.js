0b5485fb33197089e6c1c0193e2bb2321497b3ff
Amlan <amlannandy5@gmail.com>
2020-10-28 18:58:31 +0530

    Fixed warnings

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -8,7 +8,11 @@ const PageNotFound = () => {
   return (
     <div>
       <h1 className='display-4 mt-5'>Page Not Found</h1>
-      <img src={PageNotFoundImage} style={{ height: '300px' }} />
+      <img
+        src={PageNotFoundImage}
+        alt='This Page is not available'
+        style={{ height: '300px' }}
+      />
       <br />
       <Button className='ml-3' as={Link} to='/' variant='warning'>
         Go to Home
